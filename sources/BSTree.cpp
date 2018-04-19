7bdb02529114ba3e99f99134735b02e1c83f76fa
rasulbabaev <32385063+rasulbabaev@users.noreply.github.com>
2018-04-20 00:42:54 +0300

    Update BSTree.cpp

--- sources/BSTree.cpp
+++ sources/BSTree.cpp
@@ -19,7 +19,7 @@ void MenuOut()
 	cout << "6. Загрузить дерево из файла" << endl;
 	cout << "7. Проверить наличие узла" << endl;
 	cout << "8. Завершение работы" << endl << endl;
-	
+
 }
 
 int main(int argc, char * argv[])
@@ -28,74 +28,95 @@ int main(int argc, char * argv[])
 	Tree *tree = new Tree;
 	for (int i = 0; i < argc; i++)
 	{
-		int x;
-		x = atoi(argv[i]);
-		tree->addel(x);
+		tree->add_element(atoi(argv[i]));
 	}
-	label:
-	MenuOut();
 
-	unsigned int ch;
-	cin >> ch;
-	switch (ch)
+	while (true)
 	{
-	case 1:
-	{
-		break;
-	}
-	case 2:
-	{
-		break;
-	}
-	case 3:
-	{
-		break;
-	}
-	case 4:
-	{
-		break;
-	}
-	case 5:
-	{
-		break;
-	}
-	case 6:
-	{
-		break;
-	}
-	case 7:
-	{
-		break;
-	}
-	case 8:
-	{
-		string exit;
-	label_exit:
-		cout << "Вы хотите выйти?" << endl;
-		cin >> exit;
-		if ((exit == "y") || (exit == "Y") || (exit == "yes") || (exit == "Yes"))
+		MenuOut();
+		unsigned int ch;
+		cin >> ch;
+		switch (ch)
 		{
+		case 1:
+		{
+			tree->print_tree();
+			delete tree;
+			break;
+		}
+		case 2:
+		{
+			char choose;
+			cout << "a - Прямой обход" << endl;
+			cout << "b - Поперечный обход" << endl;
+			cout << "c - Обратный обход" << endl;
+			cin >> choose;
+
+			if (choose == 'a')
+			{
+				tree->get_straight();
+			}
+
+			if (choose == 'b')
+			{
+				tree->get_symmetric();
+			}
+
+			if (choose == 'c')
+			{
+				tree->get_inverce();
+			}
+			break;
+		}
+		case 3:
+		{
+			break;
+		}
+		case 4:
+		{
+			break;
+		}
+		case 5:
+		{
+			break;
+		}
+		case 6:
+		{
+			break;
+		}
+		case 7:
+		{
+			break;
+		}
+		case 8:
+		{
+			string exit;
+			cout << "Вы хотите выйти?" << endl;
+			cin >> exit;
+			if ((exit == "y") || (exit == "Y") || (exit == "yes") || (exit == "Yes"))
+			{
+				return 0;
+			}
+
+			if ((exit == "n") || (exit == "N") || (exit == "no") || (exit == "No"))
+			{
+				cout << endl;
+				break;
+			}
+
+			else
+			{
+				cout << "Неверный ввод" << endl << endl;
+			}
+			break;
+		}
+		default:
+		{
+			cout << "Неверный ввод" << endl << endl;
 			return 0;
 		}
 
-		if ((exit == "n") || (exit == "N") || (exit == "no") || (exit == "No"))
-		{
-			cout << endl;
-			goto label;
 		}
-
-		else
-		{
-			cout << "Неверный ввод" << endl << endl;
-			goto label_exit;
-		}
-		break;
 	}
-	default:
-	{
-		cout << "Неверный ввод" << endl << endl;
-		goto label;
-	}
-
-	}
+	
 }
