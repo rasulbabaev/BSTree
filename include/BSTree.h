f4e70ea0dcbbb434a35213d0460eabc3197b784a
rasulbabaev <32385063+rasulbabaev@users.noreply.github.com>
2018-04-20 01:04:53 +0300

    Update BSTree.h

--- include/BSTree.h
+++ include/BSTree.h
@@ -10,7 +10,42 @@ namespace BSTree
 	class Tree //Класс дерева
 	{
 		Node * root = nullptr;
-		void print_tree(Node *root, int space)
+		void add_node(int data, Node *&NTree) //Функция добавления узла в дерево (NTree == key) 
+		{
+			if (NTree == nullptr) //Если узлов дерева нет
+			{
+				NTree = new Node; //Создаём новый узел
+				*NTree = { data, nullptr, nullptr };
+			}
+
+			if (data < NTree->data) //Если добавленный эл-т меньше
+			{
+				if (NTree->Left != nullptr) // Если узел НЕ содержит пустого указателя налево
+				{
+					add_node(data, NTree->Left); //Добавляем данные и указатель на левый узел
+				}
+				else
+				{
+					NTree->Left = new Node; //Иначе делаем новый узел
+					*NTree = { data, nullptr, nullptr };
+				}
+			}
+
+
+			if (data > NTree->data) //Если больше текущего узла
+			{ //Всё то же самое, на левую ветвь
+				if (NTree->Right != nullptr)
+				{
+					add_node(data, NTree->Right);
+				}
+				else
+				{
+					NTree->Right = new Node;
+					*NTree = { data, nullptr, nullptr };
+				}
+			}
+		}
+		void print_tree(Node *root, int space) 
 		{
 			if (root == nullptr)
 			{
@@ -27,62 +62,92 @@ namespace BSTree
 				std::cout << "	";
 			}
 			std::cout << "--" << root->data << std::endl;
-
 			print_tree(root->Left, space);
 		}
-		void add_node(int data, Node *&NTree) //Функция добавления узла в дерево
+		void straight(Node * root)
 		{
-			if (NTree == nullptr) //Если узлов дерева нет
+			if (root == nullptr)
 			{
-				NTree = new Node; //Создаём новый узел
-				NTree->data = data; //Записываем туда значения
-				NTree->Left = nullptr; //Делаем пустые указатели от этого узла налево и направо
-				NTree->Right = nullptr;
+				std::cout << "Дерево пусто" << std::endl;
+				return;
 			}
 
-			if (data < NTree->data) //Если добавленный эл-т меньше
+			if (root == nullptr)
 			{
-				if (NTree->Left != nullptr) // Если узел НЕ содержит пустого указателя налево
-				{
-					add_node(data, NTree->Left); //Добавляем данные и указатель на левый узел
-				}
-				else
-				{
-					NTree->Left = new Node; //Иначе делаем новый узел
-					NTree->Left->Left = nullptr; //Левую ветвь левого элемента
-					NTree->Left->Right = nullptr; //Правую ветвь левого элемента
-					NTree->Left->data = data; //Записываем данные в левый узел
-				}
+				std::cout << "Дерево пусто" << std::endl;
+				return;
 			}
-
-
-			if (data > NTree->data) //Если больше текущего узла
-			{ //Всё то же самое, на левую ветвь
-				if (NTree->Right != nullptr)
-				{
-					add_node(data, NTree->Right);
-				}
-				else
-				{
-					NTree->Right = new Node;
-					NTree->Right->Right = nullptr;
-					NTree->Right->Left = nullptr;
-					NTree->Right->data = data;
-				}
+			std::cout << root->data << " ";
+			if (root->Left != nullptr)
+			{
+				straight(root->Left);
+			}
+			if (root->Right != nullptr)
+			{
+				straight(root->Right);
 			}
 		}
+		void symmetric(Node *root)
+		{
+			if (root == nullptr)
+			{
+				std::cout << "Дерево пусто" << std::endl;
+				return;
+			}
+
+			if (root->Left != nullptr)
+			{
+				symmetric(root->Left);
+			}
+
+			std::cout << root->data << " ";
+
+			if (root->Right != nullptr)
+			{
+				symmetric(root->Right);
+			}
+		}
+		void inverse(Node *root)
+		{
+			if (root == nullptr)
+			{
+				std::cout << "Дерево пусто" << std::endl;
+				return;
+			}
+
+			if (root->Left != nullptr)
+			{
+				inverse(root->Left);
+			}
+
+			if (root->Right != nullptr)
+			{
+				inverse(root->Right);
+			}
+
+			std::cout << root->data << " ";
+		}
 	public:
-
-
+		void get_straight ()
+		{
+			straight(root);
+		}
+		void get_symmetric ()
+		{
+			symmetric(root);
+		}
+		void get_inverce ()
+		{
+			inverse(root);
+		}
 		void add_element(int data) //Функция добавления элемента в узел
 		{
 			add_node(data, root);
 		}
-
-		void print_tree()
+		void print_tree() 
 		{
 			print_tree(root, 0);
 		}
 	};
-}
 
+}
